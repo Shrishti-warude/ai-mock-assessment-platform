90d61e8293e29ca2405d1226d54fa1d53be0a0ac
Shrishti warude <shrishtiwarude24@gmail.com>
2026-09-25 23:38:04 +0530

    Added and tested authMiddleware for protected routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const {registerUser , loginUser, refreshAccessToken} = require('../controllers/authController');
+const { protect } = require('../middlewares/authMiddleware');
+
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login' , loginUser);
 router.post('/refresh' , refreshAccessToken);
+
+router.get('/me' , protect , (req , res) => {
+    res.json(req.user);
+});
 
 module.exports = router;
